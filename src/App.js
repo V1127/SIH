b3251ae72a9b25888c779469d241ffd7b1c387f4
V1127 <111186746+V1127@users.noreply.github.com>
2023-09-22 20:23:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Singup"; 
+import Login from "./components/Login";
+import Home from "./components/home/home"; 
+
+
+function App() {
+  const user = localStorage.getItem("token");
+
+  return (
+    <Routes>
+      <Route path="/" element={user ? <Main /> : <Navigate to="/login" />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+	  <Route path="/home" element={<home />} />
+
+    </Routes>
+  );
+}
+
+export default App;
